c206c04cd3f6ef2c0d90531ef3318299d1628762
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-07-29 18:23:23 +0100

    allow all

--- newproject/settings.py
+++ newproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@)@)#uu7)f4fiw6hn8teg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
