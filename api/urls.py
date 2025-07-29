d7fdf10d36bdcc132e80e0d91b7942be79350c95
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-07-29 19:40:23 +0100

    remove trailing slash

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import get_books, create_book, book_details
 
 urlpatterns = [
-    path('books/', get_books, name='get_books'),
-    path('books/create/', create_book, name='create_book'),
-    path('books/<int:pk>/', book_details, name='book_details'),
+    path('books', get_books, name='get_books'),
+    path('books/create', create_book, name='create_book'),
+    path('books/<int:pk>', book_details, name='book_details'),
 ]
  
